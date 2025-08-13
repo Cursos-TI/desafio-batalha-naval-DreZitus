756336286322cf6a5525bebb75172b0d9cc47536
André <108902451+DreZitus@users.noreply.github.com>
2025-08-13 02:22:33 +0000

    incializando codigo meste

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,53 +4,47 @@
 #define LINHAS 10
 #define COLUNAS 10
 
-// Matriz do tabuleiro
-int tabuleiro[LINHAS][COLUNAS];
+// Definição de valores padrão das habiliades
+#define CON 1
+#define OCT 2
+#define CRU 3
 
-int main() {
-    // 1. Inicializa o tabuleiro com "água" (0)
+
+
+void habilidadeCone(){
+    
+    for (int i = 0; i < CON; i++)
+    {
+        for (int j = 0; j < CON; j++)
+        {
+            if ()
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+    
+}
+
+void imprimeTabuleiro(int tabuleiro[LINHAS][COLUNAS]){
+
+    //Inicializa o tabuleiro com "água" (0)
     for (int i = 0; i < LINHAS; i++) {
         for (int j = 0; j < COLUNAS; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // 2. Posiciona navio horizontal de tamanho 3 (valor 3 no tabuleiro)
-    int linha_navio_h = 2;          // Linha 3 (índice 2)
-    int col_inicio_navio_h = 3;     // Coluna D (índice 3)
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[linha_navio_h][col_inicio_navio_h + i] = 3;
-    }
-
-    // 3. Posiciona navio vertical de tamanho 3 (valor 3 no tabuleiro)
-    int linha_inicio_navio_v = 4;   // Linha 5 (índice 4)
-    int col_navio_v = 1;            // Coluna B (índice 1)
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[linha_inicio_navio_v + i][col_navio_v] = 3;
-    }
-
-    // 3.a Navio diagonal principal (tamanho 3): (i+k, j+k)
-    int linhaDiagonal1 = 0;  // linha inicial
-    int colunaDiagonal1 = 0;  // coluna inicial
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] = 3; // (0,0),(1,1),(2,2)
-    }
-
-    // 3.b Navio diagonal secundária (tamanho 3): (i+k, j-k)
-    int linhaDiagonal2 = 0;  // linha inicial
-    int colunaDiagonal2 = 9;  // coluna inicial (extremo direito)
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[linhaDiagonal2 + i][colunaDiagonal2 - i] = 3; // (0,9),(1,8),(2,7)
-    }
-
-    // 4. Imprime cabeçalho das colunas
+    // Imprime cabeçalho das colunas
     printf("   ");
     for (char letra = 'A'; letra < 'A' + COLUNAS; letra++) {
         printf("%c ", letra);
     }
     printf("\n");
 
-    // 5. Imprime o tabuleiro
+    // Imprime o tabuleiro
     for (int i = 0; i < LINHAS; i++) {
         printf("%2d ", i + 1);
         for (int j = 0; j < COLUNAS; j++) {
@@ -58,6 +52,12 @@ int main() {
         }
         printf("\n");
     }
+}
+
+int main() {
+    int tabuleiro[LINHAS][COLUNAS];
+    imprimeTabuleiro(tabuleiro);
+    
 
     return 0;
 }
